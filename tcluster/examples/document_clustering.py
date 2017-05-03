7acc654790405a1f481365ae8dac363b153e3dcd
Flavio Martins <flaviomartins@acm.org>
2017-05-03 10:44:53 +0200

    cluster the whole 20newsgroups dataset

--- tcluster/examples/document_clustering.py
+++ tcluster/examples/document_clustering.py
@@ -122,7 +122,7 @@ categories = [
     'sci.space',
 ]
 # Uncomment the following to do the analysis on all the categories
-#categories = None
+categories = None
 
 print("Loading 20 newsgroups dataset for categories:")
 print(categories)
@@ -186,10 +186,10 @@ if opts.n_components:
 # Do the actual clustering
 
 if opts.sample:
-    km = SampleKMeans(n_clusters=true_k, init='k-means++', max_iter=10, n_init=1, metric=opts.metric, a=.7,
+    km = SampleKMeans(n_clusters=true_k, init='k-means++', max_iter=50, n_init=1, metric=opts.metric, a=.4,
                       init_size=None, verbose=2 if opts.verbose else 0)
 else:
-    km = KMeans(n_clusters=true_k, init='k-means++', max_iter=10, n_init=1, metric=opts.metric, a=.7,
+    km = KMeans(n_clusters=true_k, init='k-means++', max_iter=50, n_init=1, metric=opts.metric, a=.4,
                 verbose=2 if opts.verbose else 0)
 
 print("Clustering sparse data with %s" % km)
