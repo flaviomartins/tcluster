7e766e94ce039f4cf6585a0143e0a6806df8e81e
Flavio Martins <flaviomartins@acm.org>
2017-05-15 19:09:19 +0100

    fix relative import of cython functions

--- tcluster/metrics/jsd.py
+++ tcluster/metrics/jsd.py
@@ -6,7 +6,7 @@ from scipy.special import rel_entr
 import pyximport
 pyximport.install(setup_args={"include_dirs": np.get_include()},
                   reload_support=True)
-from . import js_div
+from .js_div import js_div
 
 
 def jensen_shannon_divergence(X, Y):
